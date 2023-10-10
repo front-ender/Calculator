12507f4aa29a574940a78d915f1205bd42331e64
front-ender <richard.john.gascoigne@gmail.com>
2023-10-10 14:11:25 +0200

    Add Division to Calculator Controller

--- Calculator/Controllers/CalculatorController.cs
+++ Calculator/Controllers/CalculatorController.cs
@@ -28,5 +28,44 @@ namespace Calculator.Controllers
                 Result = request.Number1 + request.Number2,
             };
         }
+
+
+
+        /// <summary>
+        /// Division - divide Number1 by Number2
+        /// Throws DivideByZeroException
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns>Result as a decimal</returns>
+        [Route("Division")]
+        [HttpPost()]
+        [ProducesResponseType(typeof(CalculationResult), 200)]
+        [ProducesResponseType(typeof(string), 400)]
+        public async Task<ActionResult> Division([FromBody] CalculationRequest request)
+        {
+            var filelogger = new FileLogger(new FileInfo(new StringBuilder(Environment.CurrentDirectory).Append(CalculatorFile).ToString()));
+            var consolelogger = new ConsoleLogger();
+
+            var logEntry = $"Dividing {request.Number1} by {request.Number2}";
+            var _ = await filelogger.LogEntry(logEntry);
+            _ = await consolelogger.LogEntry(logEntry);
+
+            try
+            {
+                decimal result = request.Number1 / request.Number2;
+                return Ok(new CalculationResult
+                {
+                    Result = result,
+                });
+            }
+            catch (DivideByZeroException)
+            {
+                return BadRequest("Divide by zero error");
+            }
+            catch
+            {
+                return BadRequest("The operation is invalid");
+            }
+        }
     }
 }
